566dde3a4ce244c8654cc0b4101b13e3d67c795d
cherylq26 <galaxy_2626@hotmail.com>
2016-09-02 10:33:43 +0800

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("Hello world again!");
-		System.out.println("It's Friday!");
+		System.out.println("It's Friday!!");
 		System.exit(0);
 	}
 }
